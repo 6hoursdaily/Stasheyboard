30acfc0ad7b1b4730fd287530618f71cc09c6f80
Charlie Gaines <beaugaines@gmail.com>
2012-07-05 15:51:37 -0400

    vergin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stasheyboard::Application.config.secret_token = 'c7ede23d85b9a1b8eb20d2e135ec5000f32ef709bd189070ec4f6768dbb510d6af96962b545af2aaf4c70503be62fa210d9e281b30fcef3d760c4930167cae01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Stasheyboard::Application.config.session_store :cookie_store, key: '_stasheyboard_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Stasheyboard::Application.config.session_store :active_record_store
