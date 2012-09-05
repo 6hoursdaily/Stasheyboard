be945221daf28f2bfc1e23b41f45d003ab99890c
Charlie Gaines <beaugaines@gmail.com>
2012-09-04 21:58:31 -0400

    styline nearly done, wow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,20 @@
 module ApplicationHelper
 
+  # def date_presenter
+  #   content = []
+  #   (1..6).each do |x|
+  #     content << "%th (#{Date.today - x}).strftime(\"%A. %d\")"
+  #     content
+  #   end
+  # end
+
   def date_presenter
-    content = []
-    (1..6).each do |x|
-      content << "%th (#{Date.today - x}).strftime(\"%A. %d\")"
+    contents = []
+    content_tag :th do
+      (1..6).each do |x|
+        contents << (Date.today - x).strftime("%a. %d")
+        contents
+      end
     end
   end
-
 end
