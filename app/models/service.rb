d855fa62ae188b4ce62e46f03acf47ae48575a5e
Charlie Gaines <beaugaines@gmail.com>
2012-09-12 18:16:36 -0400

    updated readme

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,8 +1,5 @@
 class Service < ActiveRecord::Base
-<<<<<<< .merge_file_WFkQOo
   attr_accessible :name
-=======
   attr_accessible :name, :status
->>>>>>> .merge_file_TPGnMi
   has_many :statuses
 end
