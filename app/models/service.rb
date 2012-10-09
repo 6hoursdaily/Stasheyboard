c04bfb1bf42d84de8bbf0f4a280f1d892701ef40
Charlie Gaines <beaugaines@gmail.com>
2012-10-09 19:29:53 -0400

    basic service model specs passing

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,5 @@
 class Service < ActiveRecord::Base
   attr_accessible :name, :status
   has_many :statuses
+  validates :name, :uniqueness => true, :presence => true
 end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Service do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "valid service" do
+    let(:service) { create(:service) }
+
+    subject { service }
+
+    it { should be_valid }
+    it { should respond_to(:name) }
+    it { should respond_to(:statuses) }
+
+
+    it "is invalid without a name" do
+      FactoryGirl.build(:service, :name => "").should_not be_valid
+    end
+
+
+    it "is invalid without a unique name" do
+      create(:service, name: "uniqueuser@cc.com")
+      FactoryGirl.build(:service, :name => "uniqueuser@cc.com").should_not be_valid
+    end
+
+  end
 end
