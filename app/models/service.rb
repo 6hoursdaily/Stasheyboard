9defeb867fb1ded840f52ed5cc1210ec38ffa9c0
Charlie Gaines <beaugaines@gmail.com>
2012-10-09 19:12:48 -0400

    added service factory

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,4 @@
 class Service < ActiveRecord::Base
-  attr_accessible :name
   attr_accessible :name, :status
   has_many :statuses
 end
---
+++ spec/factories/service_factory.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :service do
+    name "Server 1"
+  end
+end
