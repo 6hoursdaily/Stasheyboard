33d45b871450356a8ae25b9fecdaf4e9867cf205
Charlie Gaines <beaugaines@gmail.com>
2012-10-11 18:39:57 -0400

    about to write status sort by date method

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -7,5 +7,6 @@ class ServicesController < ApplicationController
   def show
     @service = Service.find(params[:id])
     @statuses = @service.statuses.last(4).reverse
+    # what I need:  @statuses = statuses_sorted_by_date
   end
 end
