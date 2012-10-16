f5c8476af41764a3ec314a733f7a10a9bf9e3248
Charlie Gaines <beaugaines@gmail.com>
2012-10-16 18:59:31 -0400

    added authorize admin method and before filter to dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class Admin::DashboardController < ApplicationController
+
+  before_filter :authorize_admin!
+
   def index
+  end
+
+  private
+
+  def authorize_admin!
+    authenticate_user!
+    unless current_user.admin?
+      flash.now[:alert] = "You must be an admin to do that."
+      redirect_to root_path
+    end
   end
 end
